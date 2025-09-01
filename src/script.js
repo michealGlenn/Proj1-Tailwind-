659e1096786473fbc201416a3328e2102416aa84
MICHEAL GLENN <tohmichael62@gmail.com>
2025-09-01 02:38:51 +0100

    Hydra website (final responsive website)
    MVP

--- src/script.js
+++ src/script.js
@@ -19,14 +19,89 @@ closeBtn.parentElement.addEventListener("click", (e) => {
 });
 
 
-const swiper = new Swiper(".swiper", {
-  loop: true, // infinite loop
+// const swiper = new Swiper(".swiper", {
+//   loop: true, // infinite loop
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
+
+// const swiper2 = new Swiper(".swiper1", {
+//   loop: true, // infinite loop
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
+
+// First swiper
+const swiper1 = new Swiper(".swiper-one", {
+  loop: true,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-one .swiper-button-next",
+    prevEl: ".swiper-one .swiper-button-prev",
   },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+   pagination: {
+   el: ".swiper-pagination",
+   clickable: true,
+ },
 });
+
+// Second swiper
+const swiper2 = new Swiper(".swiper-two", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-two .swiper-button-next",
+    prevEl: ".swiper-two .swiper-button-prev",
+  },
+     pagination: {
+   el: ".swiper-pagination",
+   clickable: true,
+ },
+});
+
+// Third swiper
+const swiper3 = new Swiper(".swiper-three", {
+  loop: true,
+  // autoplay:true,
+  navigation: {
+    nextEl: ".swiper-three .swiper-button-next",
+    prevEl: ".swiper-three .swiper-button-prev",
+  },
+     pagination: {
+   el: ".swiper-pagination",
+   clickable: true,
+ },
+});
+
+// Forth swiper
+const swiper4 = new Swiper(".swiper-four", {
+  loop: true,
+  // autoplay:true,
+  navigation: {
+    nextEl: ".swiper-four .swiper-button-next",
+    prevEl: ".swiper-four .swiper-button-prev",
+  },
+     pagination: {
+   el: ".swiper-pagination",
+   clickable: true,
+ },
+});
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 1,   // show 1 card at a time on mobile
+  spaceBetween: 1,
+});
+
+let promt = alert('Hello👋, I am Micheal Glenn. A Frontend end web developer🤗. Please click on feedback to share with me your experience on my website (open the menu button to see the feedback option on mobile devices). Thank you');
+
+console.log(promt);
